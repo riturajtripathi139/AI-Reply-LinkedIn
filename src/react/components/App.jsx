02c52fc51b04ad01642f63ad1f3da55210dbbc2f
Rituraj Tripathi <60547939+riturajtripathi139@users.noreply.github.com>
2024-10-25 13:41:21 +0530

    Update App.jsx

--- src/react/components/App.jsx
+++ src/react/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='w-[300px] h-[50px] p-4 bg-white text-blue-500'>
-      Welcome to CHATGPT AI Assistant
+      Enter Your Prompt
     </div>
   )
 }
